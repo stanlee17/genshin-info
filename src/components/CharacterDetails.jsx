7b3ccbb7fbbfa7f2c3a2d0055efed7068dbe8600
Stanlee <stanz.ferigh@gmail.com>
2021-01-26 15:00:16 +1100

    skill, passive and constellations added to UI

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -33,9 +33,45 @@ const CharacterDetails = () => {
             <h3>{characterDetails.vision}</h3>
           </div>
         </div>
-        <div className="char-details__overview">
-          <h2>{characterDetails.name} Overview</h2>
+        <div className="char-details__overview margin-bott-top-md">
+          <h1>{characterDetails.name} Overview</h1>
           <p>{characterDetails.description}</p>
+        </div>
+        <h1 className="margin-bott-top-md">
+          {characterDetails.name} Skill Talents
+        </h1>
+        <div className="char-details__skills">
+          {characterDetails.skillTalents.map((skill) => (
+            <div className="char-details__skills-content">
+              <h2>{skill.name}</h2>
+              <h3>{skill.unlock}</h3>
+              <p>{skill.description}</p>
+            </div>
+          ))}
+        </div>
+        <h1 className="margin-bott-top-md">
+          {characterDetails.name} Passive Talents
+        </h1>
+        <div className="char-details__passive">
+          {characterDetails.passiveTalents.map((passive) => (
+            <div className="char-details__passive-content">
+              <h2>{passive.name}</h2>
+              <h3>{passive.unlock}</h3>
+              <p>{passive.description}</p>
+            </div>
+          ))}
+        </div>
+        <h1 className="margin-bott-top-md">
+          {characterDetails.name} Constellations
+        </h1>
+        <div className="char-details__constellations">
+          {characterDetails.constellations.map((constellation) => (
+            <div className="char-details__constellations-content">
+              <h2>{constellation.name}</h2>
+              <h3>{constellation.unlock}</h3>
+              <p>{constellation.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     )
